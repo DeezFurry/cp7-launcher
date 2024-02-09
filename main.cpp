517dd7c5f644c700cd52d14c8033372d390b12c7
giulix <105929248+DeezFurry@users.noreply.github.com>
2024-02-09 20:49:49 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <windows.h>
 #include <conio.h>
+#include <shellapi.h>
 
 // i used a tutorial for this lmao
 
@@ -28,25 +29,29 @@ int main()
 
     for (int i = 0 ;;)
     {
-        gotoxy(10,5);
+        gotoxy(12,8);
         color(Set[0]);
         cout << "Play";
 
-        gotoxy(10,6);
+        gotoxy(12,9);
         color(Set[1]);
         cout << "Open Local Appdata";
 
-        gotoxy(10,7);
+        gotoxy(12,10);
         color(Set[2]);
+        cout << "Discord";
+
+        gotoxy(12,11);
+        color(Set[3]);
         cout << "Exit";
 
         key = _getch();
 
-        if(key == 72 && (counter >=2 && counter <=3))
+        if(key == 72 && (counter >=2 && counter <=4))
         {
             counter --;
         }
-        if(key == 80 && (counter >=1 && counter <=2))
+        if(key == 80 && (counter >=1 && counter <=3))
         {
             counter ++;
         }
@@ -62,12 +67,17 @@ int main()
             }
             if(counter == 3)
             {
+                system("start https://discord.gg/2J7VPVMFAY");
+            }
+            if(counter == 4)
+            {
                 exit(0);
             }
         }
         Set[0] = 7; // white
         Set[1] = 7;
         Set[2] = 7;
+        Set[3] = 7;
         if (counter == 1)
         {
             Set[0] = 12; // 12 = color red
@@ -80,8 +90,13 @@ int main()
         {
             Set[2] = 12; // 12 = color red
         }
+        if (counter == 4)
+        {
+            Set[3] = 12;
+        }
 
     }
 
     return 0;
 }
+
